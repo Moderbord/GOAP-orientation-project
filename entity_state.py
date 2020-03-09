e474472136ad0af5c23c2840fabc633d121a22ff
Pontus Andersson <ponane-6@student.ltu.se>
2020-03-09 18:31:59 +0100

    -Workers can now path back and forth  between a resource and home

--- entity_state.py
+++ entity_state.py
@@ -1,4 +1,6 @@
+from enum import Enum, auto
 from random import randint
+
 import message_dispatcher as dispatcher
 import game_time as time
 import custom_thread as c_thread
@@ -66,60 +68,114 @@ class StateIdle(State):
         pass
 
 class StateGather(State):
+
+    class Stage(Enum):
+        Done = auto()
+        Traversing = auto()
+        Gathering = auto()
+        Delivering = auto()
+
     def enter(self, entity):
-        pass
+        self.stage = self.Stage.Done
+        self.finding_path = False
 
     def execute(self, entity):
-        if not entity.is_traversing and not entity.is_finding_path and entity.owner.target_resource:
+        if self.stage is self.Stage.Done:
+            if not self.finding_path and entity.owner.target_resource:
+                self.finding_path = True
+                goal = entity.owner.get_resource_location(entity.owner.target_resource)
+                find_path(entity, entity.location, goal, self.__get_resource_path_callback)
 
-            entity.is_finding_path = True
-            loc = entity.location
-            goal = entity.owner.get_resource_location(entity.owner.target_resource)
-            find_path(entity, loc, goal)
+        elif self.stage is self.Stage.Traversing:
+            pass
+            # go to resource
+            # > Gathering
+
+        elif self.stage is self.Stage.Gathering:
+            if not self.finding_path: # and completed gathering
+                self.finding_path = True
+                goal = entity.owner.start_position
+                find_path(entity, entity.location, goal, self.__get_delivery_path_callback)
+            # tick progress
+            # deduct one resource from tile
+            # set carrying resource
+            # > Delivering
+
+        elif self.stage is self.Stage.Delivering:
+            pass
+            # get back to base
+
+            # > Done
 
     def exit(self, entity):
-        entity.is_traversing = False
+        pass
 
     def on_message(self, entity, message):
         if message.msg == dispatcher.MSG.ArrivedAtGoal:
-            entity.is_traversing = False
+            if self.stage is self.Stage.Traversing:
+                self.stage = self.Stage.Gathering
+                # occupy one resource at tile
+            elif self.stage is self.Stage.Delivering:
+                # increment at base
+                # remove resource from self
+                self.stage = self.Stage.Done
+
             return True
 
         return False
+
+    def __get_resource_path_callback(self, entity, result):
+        self.finding_path = False
+        if result:
+            self.stage = self.Stage.Traversing
+            entity.set_path(result)
+
+    def __get_delivery_path_callback(self, entity, result):
+        self.finding_path = False
+        if result:
+            self.stage = self.Stage.Delivering
+            entity.set_path(result)
 
 class StateExplore(State):
+
+    class Stage(Enum):
+        Traversing = auto()
+        Done = auto()
+
     def enter(self, entity):
-        pass
+        self.stage = self.Stage.Done
+        self.finding_path = False
 
     def execute(self, entity):
-        if not entity.is_traversing and not entity.is_finding_path:
-
-            entity.is_finding_path = True
+        if self.stage is self.Stage.Done and not self.finding_path:
+            self.finding_path = True
             loc = entity.location
             goal = (randint(0, entity.gamemap.tile_width - 1), (randint(0, entity.gamemap.tile_height - 1)))
-            find_path(entity, loc, goal)
+            find_path(entity, loc, goal, self.__get_path_callback)
 
     def exit(self, entity):
-        entity.is_traversing = False
+        pass
 
     def on_message(self, entity, message):
         if message.msg == dispatcher.MSG.ArrivedAtGoal:
-            entity.is_traversing = False
+            self.stage = self.Stage.Done
             return True
 
         return False
 
-def find_path(entity, location, goal):
+    def __get_path_callback(self, entity, result):
+        self.finding_path = False
+        if result:
+            self.stage = self.Stage.Traversing
+            entity.set_path(result)
+
+# Method will create a separate thread and calculate a path between two points
+def find_path(entity, location, goal, __callback):
         thread = c_thread.BaseThread(
             target=entity.gamemap.get_path,
             target_args=(location, goal),
-            callback=__get_path_callback,
+            callback=__callback,
             callback_args=[entity]
         )
         thread.start()
 
-def __get_path_callback(entity, result):
-    entity.is_finding_path = False
-    if result:
-        entity.set_path(result)
-        entity.is_traversing = True
